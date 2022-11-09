bc2267ba44c45b8ce9c09069dc3917249f6420bf
majjianass <anassmajji34@gmail.com>
2022-11-09 23:43:53 +0100

    streamlit_app

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ st.set_page_config(layout="wide")
 STREAMLIT_STATIC_PATH = str(pathlib.Path(st.__path__[0]) / "static")
 # We create a downloads directory within the streamlit static asset directory
 # and we write output files to it
-DOWNLOADS_PATH = STREAMLIT_STATIC_PATH / "downloads"
+DOWNLOADS_PATH = STREAMLIT_STATIC_PATH + "/" + "downloads"
 if not DOWNLOADS_PATH.is_dir():
     DOWNLOADS_PATH.mkdir()
 
